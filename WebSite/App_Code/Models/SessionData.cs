7e603c1c2995dbdfb0b0c8679ded211ab23ce384
Rafael Melo <rafael.melo@aztronic.com.br>
2012-02-02 15:09:44 -0200

    Inicial

---
+++ WebSite/App_Code/Models/SessionData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for SessionInfo
+/// </summary>
+public class SessionData
+{
+    public Int32 userID;
+    public String email;
+    public Boolean foo;
+}
---
+++ WebSite/MasterPage.master.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class MasterPage : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (getSession() == null)
+        {
+            HttpContext.Current.Response.Redirect(@"~/Default.aspx");
+        }
+    }
+
+    public static SessionData getSession()
+    {
+        return (SessionData)HttpContext.Current.Session["session"];
+    }
+
+}
